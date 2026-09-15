f81da5c95f46703e45747433b11be6c684dc21f2
Victor Nascimento Bakke <2266817+Gipphe@users.noreply.github.com>
2026-09-15 12:08:36 +0200

    nix: allow unfree ghc-toolchain-lib-ghc-toolchain (#502)

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -20,7 +20,13 @@ import (if system == "x86_64-darwin" then inputs.nixpkgs-2605 else inputs.nixpkg
       tricorder = (final.tricorderProject.flake { }).packages."tricorder:exe:tricorder";
       nix-hpack = final.callPackage ./package/nix-hpack.nix { };
     })
-
   ];
-  inherit (inputs.haskell-nix) config;
+  config = inputs.haskell-nix.config // {
+    allowUnfreePredicate =
+      pkg:
+      (inputs.haskell-nix.config.allowUnfreePredicate or (_: false)) pkg
+      || builtins.elem (inputs.nixpkgs.lib.getName pkg) [
+        "ghc-toolchain-lib-ghc-toolchain"
+      ];
+  };
 }
